21a831d90cb35419fb211c94647373241086bb34
Jon Hallett <jon.hallett@bristol.ac.uk>
2022-01-26 11:05:58 +0000

    Hoist main up to the top

--- wordle.py
+++ wordle.py
@@ -5,6 +5,21 @@ import re
 import sys
 
 
+def main():
+    guesses = parse_arguments()
+
+    if not guesses:
+        print('arose')
+    elif len(guesses) == 1 and guesses[0][1] == 'GGGGG':
+        print(guesses[0][0])
+    elif len(guesses) == 1 and guesses[0][1] == 'XXXXX':
+        print('until')
+    elif len(guesses) == 2 and guesses[0][1] == 'XXXXX' and guesses[1][1] == 'XXXXX':
+        print('psych')
+    else:
+        guess(guesses)
+
+
 def parse_arguments():
     if len(sys.argv) % 2 != 1:
         raise ValueError('Guesses need to be paired with responses')
@@ -13,7 +28,7 @@ def parse_arguments():
 
     arguments = iter(sys.argv[1:])
     for guess in arguments:
-        matches =  next(arguments)
+        matches = next(arguments)
         if not re.search(r'^[a-z]{5}$', guess):
             raise ValueError('Guesses should be five letter words')
         if not re.search(r'^[XYG]{5}$', matches):
@@ -59,20 +74,6 @@ def guess(guesses):
     command_string = f"grep '^{match_pattern}$' /usr/share/dict/words | grep -v '[{exclude_patten}A-Z]' | grep '[{include_patten}]'"
 
     os.system(command_string)
-
-def main():
-    guesses = parse_arguments()
-
-    if not guesses:
-        print('arose')
-    elif len(guesses) == 1 and guesses[0][1] == 'GGGGG':
-        print(guesses[0][0])
-    elif len(guesses) == 1 and guesses[0][1] == 'XXXXX':
-        print('until')
-    elif len(guesses) == 2 and guesses[0][1] == 'XXXXX' and guesses[1][1] == 'XXXXX':
-        print('psych')
-    else:
-        guess(guesses)
 
 
 main()
